6334321480548d5e2c674e2fc842de695b035f12
Collin Jilbert <cjilbert504@gmail.com>
2020-04-16 20:08:08 -0500

    add normalizing private callback methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :list
 
-  before_create do
-     self.title = title.titlecase 
-  end
+  before_validation :normalize_title, :normalize_content, on: [:create, :update]
   
+  private 
+
+  def normalize_title 
+    self.title = title.titlecase 
+  end
+
+  def normalize_content 
+    self.content = content.capitalize 
+  end
+
 end
