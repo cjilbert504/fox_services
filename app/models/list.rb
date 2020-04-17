b6d0de930b282b9e6a418937af0fb63a5ba3abac
Collin Jilbert <cjilbert504@gmail.com>
2020-04-17 15:20:53 -0500

    add name normalize to list model, modify index view

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,13 @@
 class List < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :employees, through: :tasks
+
+  before_validation :normalize_name, on: [:create, :update]
+  
+  private 
+
+  def normalize_name
+    self.name = name.titlecase 
+  end
+
 end
