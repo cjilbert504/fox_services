859fd6df8163d83c44baac18836d81d31c38f822
Collin Jilbert <cjilbert504@gmail.com>
2020-04-17 16:37:39 -0500

    build out task show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,9 @@
 class TasksController < ApplicationController
 
+    def show
+        @task = Task.find_by(id: params[:id])
+    end
+
     def create 
         @task = Task.new(task_params)
         @task.save
