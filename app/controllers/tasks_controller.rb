ac658ae7f0e557ca2b9872f3c5e53d4eec08c74c
Collin Jilbert <cjilbert504@gmail.com>
2020-04-20 13:03:59 -0500

    Shows incompleted tasks as red

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
     private 
 
     def task_params 
-        params.require(:task).permit(:title, :content, :list_id)
+        params.require(:task).permit(:title, :content, :list_id, :completed)
     end
 
     def find_task 
