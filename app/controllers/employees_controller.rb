443479bb7098ba1121909767e970e54f5e2eeb7d
Collin Jilbert <cjilbert504@gmail.com>
2020-04-24 10:44:48 -0500

    Build of new employee signup form

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,40 @@
 class EmployeesController < ApplicationController
+
+    def index 
+
+    end
+
+    def show 
+
+    end
+
+    def new 
+        @employee = Employee.new
+    end
+
+    def edit 
+
+    end
+
+    def create 
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employee_path(@employee)
+        end
+    end
+
+    def update 
+
+    end
+
+    def destroy 
+
+    end
+
+    private 
+
+    def employee_params 
+        params.require(:employee).require(:name, :email)
+    end
+
 end
