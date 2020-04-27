9dd184ff96146cefd35152fa4449be3c81d4a673
Collin Jilbert <cjilbert504@gmail.com>
2020-04-27 13:41:04 -0500

    Add ability to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
 
+    def new 
+        @employee = Employee.new
+    end
+
+    def create
+        employee = Employee.find_by(email: params[:employee][:email])
+        if employee && employee.authenticate(params[:employee][:password])
+            session[:user_id] = employee.id
+            redirect_to employee_path(employee)
+        else
+            render 'new', alert: "User not found. Please try again."
+        end
+    end
+
     def destroy
         session.delete :user_id
         redirect_to root_path
     end
+
 end
