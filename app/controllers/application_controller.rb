a368930c6577746fb4536e46f1803c720bb1deaf
Collin Jilbert <cjilbert504@gmail.com>
2020-05-01 19:57:05 -0500

    Add logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
 
+    def logged_in?
+        redirect_to new_session_path, alert: "You must be logged in to do that" unless !!session[:user_id]
+    end
 end
