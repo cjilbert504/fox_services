95106f73310a4a428bc68be2185bbad99c19783a
Collin Jilbert <cjilbert504@gmail.com>
2020-05-01 20:08:43 -0500

    Made logged_in? method private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+    
+    private
 
     def logged_in?
         redirect_to new_session_path, alert: "You must be logged in to do that" unless !!session[:user_id]
     end
+
 end
