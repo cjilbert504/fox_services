c65a4459e14619b93e10e723ce7a205c733e90e3
Collin Jilbert <cjilbert504@gmail.com>
2020-05-03 09:32:36 -0500

    Add helpers to simplify show view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,14 @@
 module TasksHelper
+    def check_task_complete 
+        @task.completed ? "Completed" : "Incomplete"
+    end
+
+    def check_if_task_assigned
+        if @task.employee != nil
+            @task.employee.name
+        else
+            "Task Is Not Assigned!"
+        end
+    end
+
 end
