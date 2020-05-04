d677f6f8021824a56cef5e05bd5a8090a3ef08dd
Collin Jilbert <cjilbert504@gmail.com>
2020-05-04 12:28:59 -0500

    Remove phone number presence validation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,5 +5,5 @@ class Employee < ApplicationRecord
 
   has_secure_password
 
-  validates :name, :email, :phone_number, :password, presence: true
+  validates :name, :email, :password, presence: true
 end
