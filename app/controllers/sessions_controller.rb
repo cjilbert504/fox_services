63f1ecb6fe8259b5fbb2d8b28ad3dfdc75048990
Collin Jilbert <cjilbert504@gmail.com>
2020-05-04 12:28:22 -0500

    Add action to login in with facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class SessionsController < ApplicationController
 
     def new 
@@ -14,9 +16,25 @@ class SessionsController < ApplicationController
         end
     end
 
+    def omni
+        @employee = Employee.find_or_create_by(email: auth[:info][:email]) do |e|
+            e.uid = auth['uid']
+            e.name = auth['info']['name']
+            e.password = SecureRandom.alphanumeric(10)
+        end   
+          session[:user_id] = @employee.id
+          redirect_to employee_path(@employee)
+    end
+
     def destroy
         session.delete :user_id
         redirect_to root_path
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
     end
 
 end
