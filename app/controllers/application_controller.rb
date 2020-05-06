67586f34f72ffdb3a3301898088865e7963e83f7
Collin Jilbert <cjilbert504@gmail.com>
2020-05-06 16:14:56 -0500

    Extract logged in method into concerns module; include module in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-    
-    private
-
-    def logged_in?
-        redirect_to new_session_path, alert: "You must be logged in to do that" unless !!session[:user_id]
-    end
-
+    include LoggedInCheck
 end
