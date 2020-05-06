a16f2cf806802ca7104d5b494b9b79b7ee51241f
Collin Jilbert <cjilbert504@gmail.com>
2020-05-06 12:19:53 -0500

    Scope method that returns list with most tasks

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,8 @@ class List < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
+  # scope :most_tasks, -> {List.joins(:tasks).select("lists.*, count(tasks.id) as tcount").group("lists.id").order("tcount DESC").limit(1)}
+
   def normalize_name
     self.name = name.strftime("%B %d, %Y")
   end
