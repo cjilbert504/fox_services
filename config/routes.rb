608a74acf296a451685dee12c54b1210e2da0a9f
Collin Jilbert <cjilbert504@gmail.com>
2020-06-07 16:59:02 -0500

    remove login via facebook, login and signup routes for employees must now be manually typed into url bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
-  get '/auth/facebook/callback', to: 'sessions#omni'
   get '/auth/google_oauth2/callback', to: 'sessions#omni'
   get '/search', to: 'tasks#search'
+  get 'login', to: 'sessions#new'
+  get 'signup', to: 'employees#new'
 
   resources :contact_messages, only: [:new, :create]
 
@@ -12,12 +13,12 @@ Rails.application.routes.draw do
     resources :tasks, only: [:index, :show, :new]
   end
   
-  resources :employees do 
+  resources :employees, except: [:new] do 
     resources :tasks, only: [:index]
   end
 
   resources :tasks, only: [:index, :edit, :create, :update, :destroy]
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:create, :destroy]
 
 end
