8b0f75f6e4449f4e634db4157804e8f8537c63cc
Collin Jilbert <cjilbert504@gmail.com>
2020-06-07 20:10:55 -0500

    add tests for creating a new employee

--- app/controllers/concerns/logged_in_check.rb
+++ app/controllers/concerns/logged_in_check.rb
@@ -3,7 +3,7 @@ module LoggedInCheck
     private
 
     def logged_in?
-        redirect_to new_session_path, alert: "You must be logged in to do that" unless !!session[:user_id]
+        redirect_to login_path, alert: "You must be logged in to do that" unless !!session[:user_id]
     end
 
 end
--- spec/features/create_employee_spec.rb
+++ spec/features/create_employee_spec.rb
@@ -13,4 +13,46 @@ RSpec.describe "Creating a new employee", type: :feature do
         expect(page).to have_content('Dustin')
     end
 
+    scenario 'Blank name field' do
+        visit signup_path
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: 'dustin@example.com'
+        fill_in 'Password', with: 'dustin'
+        fill_in 'Password confirmation', with: 'dustin'
+        click_on 'Create Employee'
+        expect(page).to have_content("Name can't be blank")
+    end
+
+    scenario 'Blank email field' do
+        visit signup_path
+        fill_in 'Name', with: 'Dustin'
+        fill_in 'Email', with: ''
+        fill_in 'Password', with: 'dustin'
+        fill_in 'Password confirmation', with: 'dustin'
+        click_on 'Create Employee'
+        expect(page).to have_content("Email can't be blank")
+    end
+
+    scenario 'Blank password field' do
+        visit signup_path
+        fill_in 'Name', with: 'Dustin'
+        fill_in 'Email', with: 'dustin@example.com'
+        fill_in 'Password', with: ''
+        fill_in 'Password confirmation', with: 'dustin'
+        click_on 'Create Employee'
+        expect(page).to have_content("Password can't be blank")
+    end
+
+    scenario 'Blank password confirmation field' do
+        visit signup_path
+        fill_in 'Name', with: 'Dustin'
+        fill_in 'Email', with: 'dustin@example.com'
+        fill_in 'Password', with: 'dustin'
+        fill_in 'Password confirmation', with: ''
+        click_on 'Create Employee'
+        expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
+    
+
 end
